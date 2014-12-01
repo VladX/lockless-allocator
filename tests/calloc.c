15b421685c0bd805a03d28f3a0cc5a94c7af491b
Влад <vvladxx@gmail.com>
2014-12-01 06:54:05 +0300

    Initial

---
+++ tests/calloc.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include <stdlib.h>
+
+void * calloc(size_t, size_t);
+
+const size_t min_size = 1024 * 1;
+const size_t max_size = 1024 * 1024;
+#define num 100
+
+int main (void)
+{
+	size_t i;
+	char * ptrs[num];
+	for (i = 0; i < num; i++)
+	{
+		size_t size = (rand() % (max_size - min_size)) + min_size;
+		ptrs[i] = calloc(size, 1);
+		memset(ptrs[i], 1, size);
+	}
+	for (i = 0; i < num; i++)
+		free(ptrs[i]);
+	return 0;
+}
---
+++ tests/realloc.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include <stdlib.h>
+
+void * realloc(void *, size_t);
+
+const size_t min_size = 1024 * 1;
+const size_t max_size = 1024 * 1024;
+#define num 100
+
+int main (void)
+{
+	size_t i;
+	char * ptrs[num];
+	for (i = 0; i < num; i++)
+	{
+		size_t size = (rand() % (max_size - min_size)) + min_size;
+		ptrs[i] = realloc(NULL, 1);
+		ptrs[i] = realloc(ptrs[i], size);
+		memset(ptrs[i], 1, size);
+	}
+	for (i = 0; i < num; i++)
+		free(ptrs[i]);
+	return 0;
+}
